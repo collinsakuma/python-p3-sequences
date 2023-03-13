af21f1daab4b47c055d5e275e83f1a0d30dd2d46
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-13 16:37:41 -0700

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = list()
+    if length >= 1:
+        fibonacci_list.append(0)
+        if length >= 2:
+            fibonacci_list.append(1)
+            if length >= 3:
+                for i in range(2, length):
+                    fibonacci_list.append(
+                        fibonacci_list[i - 1] + fibonacci_list[i -2]
+                    )
+    print(fibonacci_list)
+
+
+print_fibonacci(15)
